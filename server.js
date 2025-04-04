e2b6b7f09779427952c9f6dc46ce6258a591cc3e
Abdoulllahi <dabdoulahi@gmail.com>
2025-04-03 21:31:25 -0500

    simple get

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var http = require('http');
+const itemsJson = require('./items.json')
 
 http.createServer(function (request, response) {
     
+    response.setHeader('Content-Type', "application/json");
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     console.log("request.method = " + request.method);
-
     if (request.method == 'GET') {
+        response.setHeader('Content-Type', 'application/json');
         response.statusCode = 200;
-        response.write("<h1>Well Received!</h1>")
+        response.write(JSON.stringify(itemsJson));
+        response.end();
+        console.log("GET: returned");
+        console.log(itemsJson);
     }
 
 }).listen(3000, function () {
